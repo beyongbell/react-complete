5511ef9af9ed5ff0ccd4a94239163c93c32d3ff9
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-09-03 21:33:15 +0700

    Hide Show React

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
       { name: "Stage", age: 26 },
     ],
     otherState: "some other value",
+    showPersons: false,
   };
 
   switchNameHandler = (newName) => {
@@ -25,28 +26,32 @@ class App extends Component {
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
-        { name: 'Min', age: 28 },
+        { name: "Min", age: 28 },
         { name: event.target.value, age: 29 },
         { name: "Gate", age: 26 },
       ],
     });
-  }
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1x solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1x solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
 
-    return (
-      <div className="App">
-        <h1> Class Component </h1>
-        <p> This is really working </p>
-        <button style={style} onClick={this.switchNameHandler.bind(this, 'MIN')}> Switch Name </button>
-        <button style={style} onClick={() => this.switchNameHandler('MIN!!!')}> Switch Name </button>
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -54,7 +59,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'MIN!')}
+          click={this.switchNameHandler.bind(this, "MIN!")}
           changed={this.nameChangedHandler}
         >
           My Hobbies : Racing
@@ -63,6 +68,27 @@ class App extends Component {
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}
         />
+      </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1> Class Component </h1>
+        <p> This is really working </p>
+        <button
+          style={style}
+          onClick={this.switchNameHandler.bind(this, "MIN")}
+        >
+          Switch Name
+        </button>
+        <button style={style} onClick={() => this.switchNameHandler("MIN!!!")}>
+          Switch Name
+        </button>
+        <button style={style} onClick={this.togglePersonHandler}>
+          Toggle Persons
+        </button>
+        {persons}
       </div>
     );
   }
