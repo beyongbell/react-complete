73920dfbc448e37de7258f9c2d265e038fe6d618
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-09-03 22:22:23 +0700

    Find Person Index

--- src/App.js
+++ src/App.js
@@ -23,14 +23,20 @@ class App extends Component {
     });
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { id: 1, name: "Min", age: 28 },
-        { id: 2, name: event.target.value, age: 29 },
-        { id: 3, name: "Gate", age: 26 },
-      ],
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
     });
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   deletePersonHandler = (index) => {
@@ -65,6 +71,7 @@ class App extends Component {
               name={person.name}
               age={person.age}
               click={() => this.deletePersonHandler(i) }
+              changed={(event) => this.nameChangedHandler(event, person.id)}
             />
           })}
       </div>
